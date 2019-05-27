ecd5c4c854b4018482269d8974e5d8315e94dfa1
personcoderguy1234567890 <45461898+personcoderguy1234567890@users.noreply.github.com>
2019-05-27 15:17:18 -0600

    added p5 play

--- sketch.js
+++ sketch.js
@@ -5,67 +5,104 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let ballStuff;
-let lineCh;
-let xCor;
-let yCor;
+// let ballStuff;
+// let lineCh;
+// let xCor;
+// let yCor;
+// let ballImage;
+
+// function setup() {
+//   createCanvas(400, 400);
+//   createSprite(100, 200, 50, 50);
+//   ballStuff = {
+//     x:50, 
+//     y:350,
+//     dx:5, 
+//     dy:5,
+//     size:50,
+//   };
+// }
+
+// // function preload() 
+// //   ballImage = loadImage("assets/smileyface.jpg");
+
+// function draw() {
+//   background(220);
+//   base();
+//   move();
+//   // drawSprites();
+// }
+
+// function ball() {
+//   fill("red");
+//   ellipse(ballStuff.x, ballStuff.y, ballStuff.size, ballStuff.size);
+// }
+
+// function move() {
+//   if (keyIsPressed && (key === "d")) {
+//     ballStuff.x += ballStuff.dx;
+//   }
+//   if (keyIsPressed && (key === "a")) {
+//     ballStuff.x -= ballStuff.dx;
+//   }
+//   if (keyIsPressed && (key === "w")) {
+//     ballStuff.y -= ballStuff.dy;
+//   }
+//   if (keyIsPressed && (key === "d" && key === "w")) {
+//     ballStuff.x += ballStuff.dx;
+//     ballStuff.y -= ballStuff.dy;
+//   }
+// }
+
+// function base() {
+//   fill("black");
+//   rect(0, 350, 400, 50);
+
+
+// }
+
+let ball;
 let ballImage;
 
+
+function preload() {
+  ballImage = loadAnimation('assets/smileyface.png', 'assets/stressedface.png');
+
+
+}
+
 function setup() {
-  createCanvas(400, 400);
-  createSprite(100, 200, 50, 50);
-  ballStuff = {
+  createCanvas(500, 500);
+  ball = {
     x:50, 
     y:350,
     dx:5, 
     dy:5,
-    size:50,
+    Scalar: 0.1,
+    width = ballImage.width * ball.Scalar,
+    height = ballImage.height * ball.Scalar,
   };
-  lineCh = {
-    x2: ballStuff.x + ballStuff.size/2,
-    y2: ballStuff.y + ballStuff.size/2,
-
-  };
-  xCor = ballStuff.x;
-  yCor = ballStuff.y;
 }
-
-function preload() 
-  ballImage = loadImage( , [successCallback], [failureCallback])
 
 function draw() {
-  background(220);
-  base();
+  background(255, 255, 255);
   move();
-  ball();
-  drawSprites();
-}
+  animation(ballImage, ball.x, ball.y, ball.width, ball.height);
 
-function ball() {
-  fill("red");
-  ellipse(ballStuff.x, ballStuff.y, ballStuff.size, ballStuff.size);
-  line(ballStuff.x, ballStuff.y, lineCh.x2, lineCh.y2);
 }
 
 function move() {
   if (keyIsPressed && (key === "d")) {
-    ballStuff.x += ballStuff.dx;
+    ball.x +=ball.dx;
   }
   if (keyIsPressed && (key === "a")) {
-    ballStuff.x -= ballStuff.dx;
+    ball.x -=ball.dx;
   }
   if (keyIsPressed && (key === "w")) {
-    ballStuff.y -= ballStuff.dy;
+    ball.y -=ball.dy;
   }
   if (keyIsPressed && (key === "d" && key === "w")) {
-    ballStuff.x += ballStuff.dx;
-    ballStuff.y -= ballStuff.dy;
+    ball.x +=ball.dx;
+    ball.y -=ball.dy;
   }
-}
-
-function base() {
-  fill("black");
-  rect(0, 350, 400, 50);
-
-
 }
