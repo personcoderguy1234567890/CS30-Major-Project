b50b2559d5d3e284957628ac0b8813870e6004f4
personcoderguy1234567890 <45461898+personcoderguy1234567890@users.noreply.github.com>
2019-06-04 15:16:57 -0600

    stuff

--- sketch.js
+++ sketch.js
@@ -6,164 +6,87 @@
 // - describe what you did to take this project "above and beyond"
 
 
-
-// function base() {
-//   fill("black");
-//   rect(0, 350, 400, 50);
-
-// }
-// let yLocation, yVelocity, yAcceleration;
-// let x, radius;
-// let gravity, ground;
-
-// function setup() {
-//   createCanvas(windowWidth, windowHeight);
-//   radius = 25;
-//   ground = height - radius;
-//   x = width/2;
-//   gravity = 0.1;
-//   yLocation = ground;
-//   yVelocity = 0;
-//   yAcceleration = 0;
-// }
-
-
-//   // physics
-//   yAcceleration = 0;
-//   yVelocity += gravity;
-//   if (yLocation > ground) {
-//     yLocation = ground;
-//     yVelocity = 0;
-//   }
-
-// function keyPressed() {
-//   if (key === " ") {
-//     yAcceleration = -5;
-//   }
-// }
-
 let ball, scalar;
 let ballImage;
-let gravity, ground;
+let groundLocation, ground;
 
+
+
+function preload() {
+  ballImage = loadImage("assets/face.png");
+}
 
 
 function setup() {
   createCanvas(500, 500);
   ball = new Theball();
-  ground = rect(0, 350, 400, 50);
-  gravity = -0.1;
-  scalar = 0.1;
-  ground = height - 50;
+}
+
+function draw() {
+  background("grey");
+  surface();
+  ball.display();
+  ball.update();
+  
+  // move();
 }
 
 class Theball {
   constructor() {
-    this.radius = 50;
+    this.radius = 25;
     this.x = 50;
-    this.y = height - 50;
+    this.y = height - 100;
     this.gravity = 0.1;
     this.dy = 0;
     this.dx = 0;
     this.ay = 0;
     this.ax = 0;
+    this.groundPoint = 450;
   }
-  preload() {
-    ballImage = loadImage('assets/smileyface.png');
-  }
+
   display() {
-    image(ballImage, ball.x, ball.y, ball.radius, ball.radius);
+    fill("red");
+    ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
+    // image(this.ballImage, this.x, this.y, this.radius, this.radius);
   }
   
   update() {
-    ball.y += ball.dy;
-    ball.dy += ball.ay;
+    this.dy += this.ay;
+    this.y += this.dy;
 
-    ball.x += ball.dx;
-    ball.dx += ball.ax;
+    this.dx += this.ax;
+    this.x += this.dx;
 
-    ball.dy = 0;
-    ball.ay = 0;
+    // this.dy = 0;
+    this.ay = 0;
 
-    ball.dx = 0;
-    ball.ax = 0;
+    // this.dx = 0;
+    this.ax = 0;
 
   }
 }
 
-
-
-
-function draw() {
-  background("grey");
-
-  ball.preload();
-  ball.display();
-  ball.update();
-  move();
+function surface() {
+  rect(0, 450, 500, 50);
+  
 }
 
 
-
-function move() {
-  ball.ay = 0;
-  
+function keyPressed() {
+  groundLocation = 400;
   if (keyIsPressed && (key === "d")) {
-    ball.ax = 5;
-
+    ball.ax = 0.5;
   }
   if (keyIsPressed && (key === "a")) {
-    ball.dx -= ball.ax;
-    ball.x -= ball.dx;
+    ball.ax = -0.5;
   }
-  
-  while (ball.dy > ground) {
-    ball.y += gravity;
-  }
-
   if (keyIsPressed && (key === "w")) {
-    ball.dy -= ball.ay;
-    ball.y -= ball.dy;
-
+    ball.ay = -5;
+    while (ball.dy > groundLocation) {
+      ball.ay += ball.gravity;
+    }
   }
-  // if (keyIsPressed && (key === "d" && key === "w")) {
-  //   ball.x += ball.dx;
-  //   ball.y -= ball.dy;
-  // }
+
 }
-
-
-
-
-
-
-
-
-// function rotationScan() {
-//   if (keyIsPressed && (key === "d")) {
-//     rotate(smileyBall.dx * , 3);
-//   }
-//   else {
-//     translate(smileyBall.x, smileyBall.y);
-//   }
-// }
-
-// function rotating(ball) {
-//   push();
-//   rotationScan();
-//   pop();
-// }
-// function move() {
-//   if(keyDown(LEFT_ARROW)) {
-//     smileyBall.x -= smileyBall.dx;
-
-//   }
-//   if(keyDown(RIGHT_ARROW)) { 
-//     smileyBall.x += smileyBall.dx;
-
-//   }
-// }
-
-
 
 
